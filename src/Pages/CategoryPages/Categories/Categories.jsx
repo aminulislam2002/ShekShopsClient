bb04af216487a437f0b665236ad900728ed5b4d5
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-03-07 15:57:00 +0600

    design product on catetgories page

--- src/Pages/CategoryPages/Categories/Categories.jsx
+++ src/Pages/CategoryPages/Categories/Categories.jsx
@@ -1,21 +1,58 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ProductCard from "../../HomePage/HomePageCard/ProductCard/ProductCard";
 
 const Categories = () => {
   const { state } = useLocation();
   const category = state?.category || "all";
+  const [products, setProducts] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  console.log(category);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/products.json");
+        const data = await response.json();
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching product data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleFavoriteClick = (productId) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.includes(productId) ? prevFavorites.filter((id) => id !== productId) : [...prevFavorites, productId]
+    );
+  };
+
+  const filteredProducts =
+    category.toLowerCase() === "all"
+      ? products
+      : products.filter((product) => product.category.toLowerCase() === category.toLowerCase());
+
   return (
     <div className="container mx-auto">
-      {category ? (
-        <>
-          <h1>
-            Categories <span className="text-2xl text-[#00cc99]">{category}</span>
-          </h1>
-        </>
+      {loading ? (
+        <h1>Loading...!</h1>
       ) : (
         <>
-          <h1>Loading...!</h1>
+          <div className="grid gap-3 md:gap-4 lg:gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10">
+            {filteredProducts.map((product) => (
+              <ProductCard
+                key={product?.id}
+                id={product?.id}
+                product={product}
+                handleFavoriteClick={handleFavoriteClick}
+                favorites={favorites}
+              ></ProductCard>
+            ))}
+          </div>
         </>
       )}
     </div>
